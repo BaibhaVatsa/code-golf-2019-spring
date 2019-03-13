03d6e644e01b7d0c75fcbe88a3cefe7f55d977c0
baibhavatsa <baibhavvatsa@gmail.com>
2019-03-12 20:45:08 -0500

    problem 2

--- solutions/problem2.py
+++ solutions/problem2.py
@@ -1,3 +1,3 @@
-a,b,c=bin(int(input()))[2:],bin(int(input()))[2:]
+a,b=bin(int(input()))[2:],bin(int(input()))[2:]
 f=[1 if a[i]!=b[i] else 0 for i in range(len(a) if len(a)<len(b) else len(b))]
 print(sum(f))
